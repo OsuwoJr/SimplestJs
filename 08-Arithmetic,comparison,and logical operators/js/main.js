9eed3af7365a82ca7958744a0e683a626871cc57
OsuwoJr <johnfredosuwo@gmail.com>
2024-01-23 10:41:10 +0300

    Arithmetic,Comparison and logical operators
    Javascript operators simply explained.

---
+++ 08-Arithmetic,comparison,and logical operators/js/main.js
@@ -0,0 +1,34 @@
+function showOutput() {
+    //Arithmetic Operators:
+    let x = 10;
+    let y = 4;
+    
+    console.log(x + y); // Addition: 14
+    console.log(x - y); // Subtraction: 6
+    console.log(x * y); // Multiplication: 40
+    console.log(x / y); // Division: 2.5
+    console.log(x % y); // Modulus (Remainder): 2
+    console.log(x ** y); // Exponentiation: 10000
+    
+    //Comparison Operators:
+    let a = 5;
+    let b = 10;
+
+console.log(a > b);  // Greater than: false
+console.log(a < b);  // Less than: true
+console.log(a >= b); // Greater than or equal to: false
+console.log(a <= b); // Less than or equal to: true
+console.log(a === b); // Equal to: false
+console.log(a !== b); // Not equal to: true
+
+//Logical Operators:
+let p = true;
+let q = false;
+
+console.log(p && q); // Logical AND: false
+console.log(p || q); // Logical OR: true
+console.log(!p);     // Logical NOT: false
+
+
+}
+
